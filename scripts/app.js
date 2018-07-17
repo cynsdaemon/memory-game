e1019092942fab304de7bf6e2947aa3e02caeee5
cynsdaemon <cynsdaemon@users.noreply.github.com>
2018-07-16 22:34:03 -0800

    add setTimeout for cards array

--- scripts/app.js
+++ scripts/app.js
@@ -1,75 +1,60 @@
 // DOM References:
 const deck = document.querySelector(".deck");
-const cards = document.getElementsByClassName("card");
 const icons = document.querySelectorAll("i");
+const cards = document.getElementsByClassName("card");
+
 
 // event listener for card deck
 deck.addEventListener("click", function(){
 	const cardTarget = event.target;
 		open(cardTarget);
-		// To Match cards: 
-		
-			// store flipped cards in an array
-			let flippedCards = [];
-						
-			for(let card of cards) {
-				if(card.classList.contains("show")) {
-					
-					flippedCards.push(card);
-					console.log(flippedCards);
-				} 
-							
-				// TODO: check flippedCards array for match
-			
-					if((flippedCards.length === 2) && (flippedCards[0].firstElementChild.classList.value === flippedCards[1].firstElementChild.classList.value)) {
-						flippedCards[0].classList.add("match");
-						flippedCards[1].classList.add("match");
-						flippedCards = [];
-					} 
-
-					// if(flippedCards[0].classList.contains("match") || flippedCards[1].classList.contains("match") === true) {
-					// 	flippedCards = [];
-					// }
-					
-					// TODO: if no match
-					// if(!flippedCards[0].firstElementChild.classList.value === flippedCards[1].firstElementChild.classList.value) {
-					// 	flippedCards[0].classList.remove("match");
-					// 	flippedCards[1].classList.remove("match");
-					// }
-
-					
-				
-				// if flipped cards equal 2, TODO: add empty array timeout
-				// if(flippedCards.length === 3) {
-				// 	flippedCards = [];
-				// }
-			
-			}
-
-			console.log(flippedCards.length);
-
-				
-		
-				
-
-				
-				
-            
-		
-
+		matchCards();				
 }, false);
 
 // When clicked, open/show the card
 function open(cardTarget){		
 	if(cardTarget.tagName === "LI") {
-		cardTarget.classList.toggle("open");
-		cardTarget.classList.toggle("show");
+		cardTarget.classList.add("open", "show");
 
 	} 
 }
 
+function matchCards() {
+	// store flipped cards in an array
+	let flippedCards = [];
+		// If card is open, add to array 			
+		for(let card of cards) {
+			if(card.classList.contains("open") && card.classList.contains("show")){
+				flippedCards.push(card);
+			}
+			
+			if(flippedCards === 2 && flippedCards[0].firstElementChild.classList.value === flippedCards[1].firstElementChild.classList.value){
+				flippedCards[0].classList.add("match");
+				flippedCards[1].classList.remove("match");
+			}
+			// If match, remove open & show
+			if(flippedCards === 2 && flippedCards[0].classList.contains("match") && flippedCards[1].classList.contains("match")) {
+				flippedCards[0].classList.remove("open", "show");
+				flippedCards[1].classList.remove("open", "show");
+			} else { 
+				close(flippedCards);
+			}
+		}
+		
+}
+
+function close(flippedCards){
+	if(flippedCards.length === 2) {
+		setTimeout(function(){
+			flippedCards[0].classList.remove("open", "show");
+			flippedCards[1].classList.remove("open", "show");
+			flippedCards = [];
+		}, 1000);
+											
+	}
+}
+
 // Per project rubric:
-// TODO: Add function that compares open cards in an array
 // TODO: Change star rating after move based on how well the player is doing in game
 // TODO: add game timer that starts when the game begins
 // TODO: add Player move counter
